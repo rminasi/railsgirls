e1725de1eb941dd98c351c333ee17225046aaf70
Raquel Minasi <raquelminasi@gmail.com>
2019-11-05 21:08:20 +0000

    pages controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/info'
   root to: redirect('/ideas')
+  get 'pages/info'
   resources :ideas
+  # resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
